6c2574d0f526fe66b1146fa87442866bb7467d2e
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-22 16:23:53 -0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -4,6 +4,11 @@ MIN_SIG = 17000
 MAX_SIG = 19000
 BIN_SIZE = math.floor((MAX_SIG - MIN_SIG) / 32)
 
-def snd(msg = "")
-    if type(msg) != "<type 'str'>":
-        
+def getSignal(num):
+    if num == None || !isinstance(num, int) || int < 0 || int > 31:
+        raise Exception("
+
+def snd(msg)
+    if msg == None || !isinstance(msg, str):
+        raise Exception("Could not send type " + type(msg))
+    
